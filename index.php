5496a8ada6c37eca4927a17832236ce488894f72
Matula José Alberto <chancilsonsmart@gmail.com>
2020-08-07 13:14:53 +0000

    it's okay now
    but some is missing

--- index.php
+++ index.php
@@ -8,8 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Ajax Shopping Cart by using Bootstrap Popover</title>
     <script src="js/jquery.min.js"></script>
-    <link rel="stylesheet" href="css/
-    bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <style>
